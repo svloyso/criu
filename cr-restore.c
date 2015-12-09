2332b1770fa6aea5580f8e1cfe569e2e6d8b2a40
svloyso <svloyso@gmail.com>
2015-12-09 10:14:03 +0300

    .

--- cr-restore.c
+++ cr-restore.c
@@ -1995,7 +1995,7 @@ int cr_restore_tasks(void)
 	if (criu_signals_setup() < 0)
 		goto err;
 
-	if(restore_tmp_files() < 0)
+	if (restore_tmp_files() < 0)
 		goto err;
 
 	ret = restore_root_task(root_item);
